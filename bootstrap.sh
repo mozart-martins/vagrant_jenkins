3c775335e4f683d94b0278a6eeecf1b160a8637a
Mozart Dias Martins <mozart.dias.martins@gmail.com>
2023-04-01 21:34:02 -0300

    New Linux Repository Signing Keys

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,13 +5,18 @@ sudo apt-get update >/dev/null 2>&1
 sudo apt-get install -y openjdk-17-jdk  >/dev/null 2>&1
 
 echo '[TASK 2] Baixa chave PGP do Jenkins'
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc >/dev/null 2>&1
+# curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+#   /usr/share/keyrings/jenkins-keyring.asc >/dev/null 2>&1
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 echo '[TASK 3] Adiciona repos de pacotes no sistema'
+# echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+#   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+#   /etc/apt/sources.list.d/jenkins.list >/dev/null 2>&1
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list >/dev/null 2>&1
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 echo '[TASK 4] Atualiza o sistema operacional e instala o Jenkins'
 sudo apt-get update >/dev/null
